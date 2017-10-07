22503d09126e2f126a1a57ce3c96c402a1fdb359
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-10-07 10:06:55 +0530

    Program to read Websites

--- udemy/01python/AllPython.py
+++ udemy/01python/AllPython.py
@@ -1,7 +1,30 @@
-someString = "This is a random String"
+import urllib2
+import sys
 
-print "the length of the string is " + str(len(someString))
+print "Test"
 
-print someString[:5]
+urlToRead = "http://www.google.com"
+crawledWebLinks = {}
 
-print someString.count("random")
+while urlToRead != '':
+    try:
+        urlToRead = input("Please enter the URL to crawl: ")
+        if urlToRead == "":
+            print "OK, exiting loop"
+            break
+        shortName = input("Please enter a short name for that url: " + urlToRead + " ")
+        webFile = urllib2.urlopen(urlToRead).read()
+        crawledWebLinks[shortName] = webFile
+    except:
+        print "******\nUnexpected Error**************", sys.exc_info()[0]
+        stopOrProceed = input("Enter 1 to stop, anything else to proceed..")
+        if stopOrProceed ==1:
+            print "Exiting the program"
+            break
+        else:
+            print "Let's Continue\n"
+            continue
+    print "Inside while loop ...outside try catch"
+print "Outside while loop "
+print crawledWebLinks.keys()
+print crawledWebLinks.values()
