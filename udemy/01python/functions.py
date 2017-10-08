a9cd83cca40acfc8d2056492574ec0395f603466
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-10-08 10:08:26 +0530

    First function in python

---
+++ udemy/01python/functions.py
@@ -0,0 +1,6 @@
+def functionThatModifiesNumber(someNumber):
+    print "Argument (input) passed in = ", someNumber
+    someNumber = someNumber + 10
+    print "Inside function after modification ", someNumber
+    return
+
