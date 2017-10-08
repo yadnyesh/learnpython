91e4b5be85560a7501478d222badbbdb417d6a31
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-10-08 10:10:19 +0530

    Functions - Modifications to numbers

--- udemy/01python/functions.py
+++ udemy/01python/functions.py
@@ -1,6 +1,11 @@
+someNumber = 10
+
 def functionThatModifiesNumber(someNumber):
     print "Argument (input) passed in = ", someNumber
     someNumber = someNumber + 10
     print "Inside function after modification ", someNumber
     return
 
+print "Value of variable before: ", someNumber
+functionThatModifiesNumber(someNumber)
+print "Value of variable after: ", someNumber
