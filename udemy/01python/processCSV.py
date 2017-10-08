aa296b7aee132fbf760304c657c15234045c7842
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-10-08 08:49:46 +0530

    Write to CSV file - add row

--- udemy/01python/processCSV.py
+++ udemy/01python/processCSV.py
@@ -18,12 +18,13 @@ with open(oneFileName, "rb") as csvFile:
         prevClose = row[7]
         tradedQty = row[9]
         pctChange = float(close) / float(prevClose) - 1
-        oneResultRow = [symbol, pctChange, float(tradedQty)]
+        oneResultRow = [symbol, pctChange, float(tradedQty), float(pctChange)]
         listOfLists.append(oneResultRow)
         print symbol, "{:,.1f}".format(float(tradedQty)/1e6) + "M INR", "{:,.1f}".format(pctChange*100)+"%"
               #symbol, "{:,.1f}".format(float(tradedQty)/1e6) + "M INR", "{:,.1f}".format(pctChange*100)+"%"
     print "Done Iterating over file....the file will be closed now!"
     print "We have information for " + str(len(listOfLists)) + " stocks"
+    print listOfLists
 
 #listOfListsSortedByQty = sorted(listOfLists, key=lambda x:x[2], reverse=True)
 listOfListsSortedByQty = sorted(listOfLists, key=lambda x:x[1], reverse=True)
@@ -35,7 +36,7 @@ import xlsxwriter
 
 excelFileName = "/home/yadnyesh/tuts/learnpython/udemy/01python/files/cm17JUL2017bhav.xlsx"
 
-workbook = xlswriter.Workbook(excelFileName)
+workbook = xlsxwriter.Workbook(excelFileName)
 worksheet = workbook.add_worksheet("Summary")
 
 worksheet.write_row("A1", ["Top Traded Stocks"])
