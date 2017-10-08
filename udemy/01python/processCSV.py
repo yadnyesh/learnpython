0fa43a4b2ad49cc464fff91acc7e35b4a62dfc4a
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-10-08 08:36:51 +0530

    Write to CSV file

--- udemy/01python/processCSV.py
+++ udemy/01python/processCSV.py
@@ -29,3 +29,19 @@ with open(oneFileName, "rb") as csvFile:
 listOfListsSortedByQty = sorted(listOfLists, key=lambda x:x[1], reverse=True)
 
 print listOfListsSortedByQty
+
+
+import xlsxwriter
+
+excelFileName = "/home/yadnyesh/tuts/learnpython/udemy/01python/files/cm17JUL2017bhav.xlsx"
+
+workbook = xlswriter.Workbook(excelFileName)
+worksheet = workbook.add_worksheet("Summary")
+
+worksheet.write_row("A1", ["Top Traded Stocks"])
+worksheet.write_row("A2", ["Stock", "%Change", "Value Traded (INR"])
+
+for rowNum in range(5):
+    oneRowtoWrite = listOfListsSortedByQty[rowNum]
+    worksheet.write_row("A" + str(rowNum + 3), oneRowtoWrite)
+workbook.close()
